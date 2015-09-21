91fd289a185a42377b22776214e82d224f28a7e0
Maxime Rainville <max@firebrand.nz>
2015-09-21 21:38:15 +1200

    Adding a license.

--- _config.php
+++ _config.php
@@ -1,2 +1,10 @@
 <?php
+/**
+ * Silverstripe module config file.
+ *
+ * @author Firebrand <hello@firebrand.nz>
+ * @license MIT
+ * @package silverstripe-excel-export
+ */
+
 define('EXCELEXPORT_DIR', ltrim(Director::makeRelative(realpath(__DIR__)), DIRECTORY_SEPARATOR));
